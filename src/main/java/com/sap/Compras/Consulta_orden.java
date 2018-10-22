c702483d390a00896aa703ed20f17c71238790cd
erick151531 <38676874+erick151531@users.noreply.github.com>
2018-10-21 19:33:34 -0500

    Update Consulta_orden.java

--- src/main/java/com/sap/Compras/Consulta_orden.java
+++ src/main/java/com/sap/Compras/Consulta_orden.java
@@ -1,9 +1,10 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Servlets.compras;
+package com.sap.Compras;
 
 import com.sap.conexion.Conexion;
 import java.io.IOException;
@@ -111,3 +112,4 @@ public class Consulta_orden extends HttpServlet {
     }// </editor-fold>
 
 }
+//nuevo
