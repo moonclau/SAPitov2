ba8c51ae53a6836a0887d28882313cb43dae3011
erick151531 <38676874+erick151531@users.noreply.github.com>
2018-10-21 19:44:26 -0500

    Update Generar_ordenCompra.java

--- src/main/java/com/sap/Compras/Generar_ordenCompra.java
+++ src/main/java/com/sap/Compras/Generar_ordenCompra.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Servlets.compras;
+package com.sap.Compras;
 
 import com.sap.conexion.Conexion;
 import java.io.IOException;
@@ -109,3 +109,4 @@ public class Generar_ordenCompra extends HttpServlet {
     }// </editor-fold>
 
 }
+
